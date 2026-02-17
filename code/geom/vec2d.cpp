cbcb9da22d7619b07645e1d03bbab14a6296255b
Kcits970 <geometricstick@gmail.com>
2026-02-18 06:32:41 +0900

    added dist function, const-ified all member functions

--- code/geom/vec2d.cpp
+++ code/geom/vec2d.cpp
@@ -3,14 +3,15 @@ struct vec2d
 	ll x, y;
 	vec2d() {}
 	vec2d(ll x, ll y): x(x), y(y) {}
-	bool operator<(vec2d a) {return x == a.x ? y < a.y : x < a.x;}
-	bool operator<=(vec2d a) {return x == a.x ? y <= a.y : x < a.x;}
-	bool operator>(vec2d a) {return a < *this;}
-	bool operator>=(vec2d a) {return a <= *this;}
-	vec2d operator+(vec2d a) {return vec2d(x+a.x, y+a.y);}
-	vec2d operator-(vec2d a) {return vec2d(x-a.x, y-a.y);}
-	vec2d operator-() {return vec2d(-x, -y);}
-	vec2d operator*(ll a) {return vec2d(a*x, a*y);}
-	ll dot(vec2d a) {return x*a.x+y*a.y;}
-	ll cross(vec2d a) {return x*a.y-y*a.x;}
+	bool operator<(vec2d a) const {return x == a.x ? y < a.y : x < a.x;}
+	bool operator<=(vec2d a) const {return x == a.x ? y <= a.y : x < a.x;}
+	bool operator>(vec2d a) const {return a < *this;}
+	bool operator>=(vec2d a) const {return a <= *this;}
+	vec2d operator+(vec2d a) const {return vec2d(x+a.x, y+a.y);}
+	vec2d operator-(vec2d a) const {return vec2d(x-a.x, y-a.y);}
+	vec2d operator-() const {return vec2d(-x, -y);}
+	vec2d operator*(ll a) const {return vec2d(a*x, a*y);}
+	ll dot(vec2d a) const {return x*a.x+y*a.y;}
+	ll cross(vec2d a) const {return x*a.y-y*a.x;}
+	ll dist(vec2d a) const {return (x-a.x)*(x-a.x)+(y-a.y)*(y-a.y);}
 };
